7750ed34c26c02041e9a43c0c26fba49b56923e4
Christopher Wahl <christopher@nothingmuch.net>
2021-07-03 11:55:49 -0400

    Project complete and passing all tests.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
 import './App.css';
+import * as marked from "marked";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+marked.setOptions({breaks: true});
+const sampleMarkdown = "# Welcome to my React Markdown Previewer!\n" +
+    "\n" +
+    "## This is a sub-heading...\n" +
+    "### And here's some other cool stuff:\n" +
+    "\n" +
+    "Heres some code, `<div></div>`, between 2 backticks.\n" +
+    "\n" +
+    "```\n" +
+    "// this is multi-line code:\n" +
+    "\n" +
+    "function anotherExample(firstLine, lastLine) {\n" +
+    "  if (firstLine == '```' && lastLine == '```') {\n" +
+    "    return multiLineCode;\n" +
+    "  }\n" +
+    "}\n" +
+    "```\n" +
+    "\n" +
+    "You can also make text **bold**... whoa!\n" +
+    "Or _italic_.\n" +
+    "Or... wait for it... **_both!_**\n" +
+    "And feel free to go crazy ~~crossing stuff out~~.\n" +
+    "\n" +
+    "There's also [links](https://www.freecodecamp.org), and\n" +
+    "> Block Quotes!\n" +
+    "\n" +
+    "And if you want to get really crazy, even tables:\n" +
+    "\n" +
+    "Wild Header | Crazy Header | Another Header?\n" +
+    "------------ | ------------- | -------------\n" +
+    "Your content can | be here, and it | can be here....\n" +
+    "And here. | Okay. | I think we get it.\n" +
+    "\n" +
+    "- And of course there are lists.\n" +
+    "  - Some are bulleted.\n" +
+    "     - With different indentation levels.\n" +
+    "        - That look like this.\n" +
+    "\n" +
+    "\n" +
+    "1. And there are numbered lists too.\n" +
+    "1. Use just 1s if you want!\n" +
+    "1. And last but not least, let's not forget embedded images:\n" +
+    "\n" +
+    "![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)\n";
+
+
+export class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            input: sampleMarkdown
+        };
+        this.updateInput = this.updateInput.bind(this);
+        this.transformToMarkdown = this.transformToMarkdown.bind(this);
+    }
+
+    updateInput(event) {
+        this.setState({
+            input: event.target.value
+        });
+    }
+
+    transformToMarkdown() {
+        return marked(this.state.input);
+    }
+
+    render() {
+        const transformed = this.transformToMarkdown();
+        return (
+            <div>
+                <section id="input">
+                    <label>Input Markdown Code Here:</label>
+                    <textarea id="editor" rows="10" value={this.state.input} onChange={this.updateInput} />
+                </section>
+                <section id="preview" dangerouslySetInnerHTML={{ __html: transformed}}>
+                </section>
+            </div>
+        );
+    }
 }
 
 export default App;
